b5e386c45eb57aba4b39e078bd80ad29e42866b4
camilo42715 <usuario@DESKTOP-BKLMN9D>
2018-08-12 21:10:23 -0500

    segundo paso

---
+++ src/calculadora/CalculadoraAritmetica.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author usuario
+ */
+public class CalculadoraAritmetica {
+
+    int operando1;
+    int operando2;
+
+    int sumar() {
+        int r;
+        r = operando1 + operando2;
+        return r;
+    }
+
+    int restar() {
+        int r;
+        r = operando1 - operando2;
+        return r;
+    }
+
+    int multiplicar() {
+        int r;
+        r = operando1 * operando2;
+        return r;
+    }
+    int dividir(){
+        int r;
+        r=operando1/operando2;
+        return r;
+    }
+}
