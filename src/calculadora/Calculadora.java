0338a43d662419823bc8bc62498660dcbbec0f98
camilo42715 <usuario@DESKTOP-BKLMN9D>
2018-08-12 21:34:58 -0500

    abance

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,6 +5,8 @@
  */
 package calculadora;
 
+import java.util.Scanner;
+
 /**
  *
  * @author usuario
@@ -15,8 +17,29 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        CalculadoraAritmetica calculadora;
+  
+        CalculadoraAritmetica micalculadora;
+        Scanner capt = new Scanner(System.in);
+        micalculadora=new CalculadoraAritmetica();
+        System.out.println("seleciio operciones: \n 1.sumar  \n 2.restar \n 3. multiplicar \n 4. dividir"); 
+        int resultado= capt.nextInt();
+        switch(resultado){
+            case 1:
+                System.out.println(micalculadora.sumar());
+                break;
+            case 2:
+                System.out.println(micalculadora.restar());
+                    break;
+            case 3:
+                 System.out.println(micalculadora.multiplicar());
+                     break;
+            case 4:
+                 System.out.println(micalculadora.dividir());
+                     break;
+            default :
+                System.out.println(" escoja una opcion");
+                
+        }
     }
-    
 }
+    
