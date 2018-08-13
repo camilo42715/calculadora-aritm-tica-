dac83132075565a6a9ceb882f8b04e33fd6e6773
camilo42715 <>
2018-08-12 22:21:39 -0500

    cambio 3

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -17,29 +17,42 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-  
+
         CalculadoraAritmetica micalculadora;
         Scanner capt = new Scanner(System.in);
-        micalculadora=new CalculadoraAritmetica();
-        System.out.println("seleciio operciones: \n 1.sumar  \n 2.restar \n 3. multiplicar \n 4. dividir"); 
-        int resultado= capt.nextInt();
-        switch(resultado){
+        micalculadora = new CalculadoraAritmetica();
+        System.out.println("primer numero ");
+        int a = capt.nextInt();
+        System.out.println("segundo numero ");
+        int b = capt.nextInt();
+        System.out.println("seleciio operciones: \n 1.sumar  \n 2.restar \n 3. multiplicar \n 4. dividir");
+
+        int resultado = capt.nextInt();
+        switch (resultado) {
             case 1:
+                micalculadora.operando1 = a;
+                micalculadora.operando2 = b;
                 System.out.println(micalculadora.sumar());
+
                 break;
             case 2:
+                micalculadora.operando1 = a;
+                micalculadora.operando2 = b;
                 System.out.println(micalculadora.restar());
-                    break;
+                break;
             case 3:
-                 System.out.println(micalculadora.multiplicar());
-                     break;
+                micalculadora.operando1 = a;
+                micalculadora.operando2 = b;
+                System.out.println(micalculadora.multiplicar());
+                break;
             case 4:
-                 System.out.println(micalculadora.dividir());
-                     break;
-            default :
+                micalculadora.operando1 = a;
+                micalculadora.operando2 = b;
+                System.out.println(micalculadora.dividir());
+                break;
+            default:
                 System.out.println(" escoja una opcion");
-                
+
         }
     }
 }
-    
