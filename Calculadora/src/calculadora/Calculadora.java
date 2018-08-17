9edafc93d48242c58f59def47d819e281789d02f
Estudiantes <Estudiantes@Labing50405.udistrital.edu.co>
2018-08-17 10:44:22 -0500

    Versión 1.2

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Programa;
+package calculadora;
 
 import calculadora.CalculadoraAritmetica;
 import calculadora.CalculadoraTrigonometrica;
